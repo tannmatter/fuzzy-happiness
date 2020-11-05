f394b70d0fb223802e60a33155fccb795d6d1888
tannmatter <tannmatter@gmail.com>
2020-11-05 14:54:22 -0600

    add/clean up documentation
    added/cleaned up a bit of docs, moved unused methods to the bottom

--- drivers/switcher/kramerp3000.py
+++ drivers/switcher/kramerp3000.py
@@ -1,7 +1,5 @@
 """
-We have two devices compatible with this protocol: a Kramer VS-42UHD 4x2 HDMI matrix switcher,
-and a Kramer VS-211UHD 2x1 HDMI switcher.  I am primarily using the VS-42 to develop on so I
-can build in support for matrix switching.
+This was tested with a VS-42UHD 4x2 HDMI matrix switcher and a VS-211UHD 2x1 HDMI switcher.
 
 Command prologue: '#'
 Response prologue: '~NN@' where NN is the ID of the device (ordinarily 01 unless reassigned)
@@ -19,38 +17,41 @@ Error codes:
         -CMD is the command sent
         -XXX is the error code
 
-    Useful codes:
+    There may or may not be one or more spaces between CMD, ERR, AND XXX.  The VS-42UHD
+    sends a space between each; the VS-211UHD does not.  So we use a regex for matching errors.
+
+    Errors we check for:
         001 - syntax error
         002 - command not available on this device
         003 - parameter out of range
-        004 - unauthorized access
-        005 - internal firmware error
-        006 - protocol busy
-        007 - wrong CRC
-        008 - timeout
-    There are many more but most fall outside the realm of what we're going to be doing.
+
+    There are a lot more but most we don't need to worry about.
 
 Observations:
-    -   The VS-42UHD works with baud 115200, however the VS-211UHD does not.  9600 worked
-        with the VS-211UHD.
-    -   The commands available differ greatly as well.
+    -   The VS-42UHD works with baud 115200 by default, however the VS-211UHD does not.
+        9600 is the only supported rate on the VS-211UHD.  I was able to set the VS-42UHD
+        to use 9600 (while connected at 115200) so we could define a default baudrate for all.
+        After sending '#BAUD 9600\r', the switcher output will stop making sense over 115200
+        (sometimes all null bytes and sometimes nothing).  Reconnecting over 9600 makes it
+        work again as expected.  The setting seems permanent, ie. it survives a power cycle.
+        You can also change this from the web interface.
+
+    -   The commands available between devices vary quite a bit.  The '#HELP' format differs
+        a bit as well.  The '#HELP' command should list every command supported by the device.
+        The manual claims you can request help on specific command by sending '#HELP COMMAND_NAME',
+        but neither of our switchers seem to support this?  Just gives a syntax error.
+
     -   Regarding routing/switching on the VS-42: the '#ROUTE?' query will always return a
-        corresponding '~01@ROUTE' response.  However a 'ROUTE' setting commmand returns a
+        corresponding '~01@ROUTE' response.  However a '#ROUTE' setting command returns a
         '~01@VID' response.  This switcher does not have separate audio inputs, and
         attempting to '#ROUTE' a layer other than video (1) will result in an error.
-        The VS-211UHD does not have the '#ROUTE' or '#ROUTE?' commands at all.  This will
-        make this a tricky driver to write.  I had planned on using the response from the
-        '#ROUTE? 1,*' command to determine how many outputs a device has, and to use that
-        information to enable matrix mode.  But now I'll need to start with that query
-        and if an '~01@ERR 002' is returned, assume that it is not a matrix switcher but
-        has a single output and that I'll need to use '#VID SRC>DEST' for switching it.
-        The alternative is somehow coding this in the configuration, or using a model list
-        or something.  All units are supposed to support '#MODEL?'
+        The VS-211UHD does not have the '#ROUTE' or '#ROUTE?' commands at all.  We use
+        '#VID' and '#VID?' for it.
 """
 
 import logging
-import select
 import re
+import select
 import sys
 
 from enum import Enum
@@ -65,7 +66,7 @@ BUFF_SIZE = 2048
 logger = logging.getLogger('KramerP3000')
 logger.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
-# set this to debug if i start misbehaving`
+# set this to debug if i start misbehaving
 ch.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 ch.setFormatter(formatter)
@@ -81,16 +82,19 @@ logger.addHandler(fh)
 class KramerP3000(SwitcherInterface):
     """This is a _very_ basic Kramer Protocol 3000 driver that works for some basic switchers.
     I know it works for our VS-211UHD and VS-42UHD. It allows for input switching and querying and
-    that's pretty much it.  Uses the #ROUTE and #VID commands.
+    that's pretty much it.
     """
     class Error(Enum):
         """Error regexes.  Varying by device, error codes may or may not contain one or more spaces
-        between 'ERR' and the code number."""
+        between 'ERR' and the code number.
+        """
         SYNTAX = rb'ERR\s*001'
         CMD_UNAVAILABLE = rb'ERR\s*002'
         PARAM_OUT_OF_RANGE = rb'ERR\s*003'
 
     class Comms(SwitcherInterface.Comms):
+        """Communication interface wrapper
+        """
         def __init__(self):
             self.connection = None
             self.serial_device = None
@@ -101,12 +105,31 @@ class KramerP3000(SwitcherInterface):
             self.ip_timeout = None
 
         def send(self, data):
+            """Send bytes
+            """
             if isinstance(self.connection, Serial):
                 return self.connection.write(data)
             elif isinstance(self.connection, socket):
                 return self.connection.send(data)
 
-        def recv(self, size=BUFF_SIZE, delay=0.3):
+        def recv(self, size: int = BUFF_SIZE, delay: float = 0.3):
+            """Receive bytes
+
+            Uses select.select() to poll for available input and keep reading until
+            the buffer is dry.
+
+            Parameters
+            ----------
+            size : int, optional
+                Number of bytes to read.  Defaults to BUFF_SIZE (2048)
+            delay : float, optional
+                How long to wait between successive reads.  Waiting longer ensures we get the whole response.
+                Defaults to 0.3
+
+            Returns
+            -------
+            The bytes read
+            """
             if isinstance(self.connection, Serial):
                 return self.connection.read(size)
             elif isinstance(self.connection, socket):
@@ -115,7 +138,6 @@ class KramerP3000(SwitcherInterface):
 
                 in_socks = [self.connection]
 
-                # we only care if input is available for this socket
                 # select called here without a timeout, so recv() blocks until there is input available
                 inputs_available, _, _ = select.select(
                     in_socks, [], []
@@ -134,12 +156,17 @@ class KramerP3000(SwitcherInterface):
                 return buffer
 
         def reset_input_buffer(self):
+            """Empty the input buffer
+
+            Clears the input buffer using the same method as recv().  select.select()
+            polls for available input until the buffer is empty, then the contents are
+            discarded.
+            """
             if isinstance(self.connection, Serial):
                 self.connection.reset_input_buffer()
             elif isinstance(self.connection, socket):
                 in_socks = [self.connection]
 
-                # we only care if input is available for this socket
                 # select called here with timeout of 0, so it polls instead of blocking!
                 ins_available, _, _ = select.select(
                     in_socks, [], [], 0
@@ -157,20 +184,42 @@ class KramerP3000(SwitcherInterface):
                             data_available = False
 
     def __init__(self, device='/dev/ttyUSB0', *, comm_method='serial', baudrate=9600, timeout=0.25,
-                 ip_address=None, ip_port=5000, ip_timeout=2.0, sw=None, inputs: int = 2):
+                 ip_address=None, ip_port=5000, sw=None, inputs: int = 2, outputs: int = None):
         """Constructor
 
-        The default means of communication is RS-232 serial as it seems the most reliable from my experiments.
+        The default means of communication is RS-232 serial as 1) it seems the most reliable from experiments, and
+        2) most switchers are going to be living inside a cabinet in close proximity to the controller.
         I have had troubles with occasional socket timeouts when using TCP/IP with these switchers, moreso than
-        with any other device. You may have to adjust the baudrate (or reconfigure your Kramer switch to use
+        with other devices. You may have to adjust the baudrate (or reconfigure your Kramer switch to use
         the lower baud) as some - like our VS-42UHD - use 115200 by default, and others - like our VS-211UHD
-        use 9600.  After device, all args should be keyword args."""
+        use 9600.  After device, all args should be keyword args.
+
+        Parameters
+        ----------
+        device : str, optional
+            The serial device to use.  Defaults to '/dev/ttyUSB0'
+        comm_method : str, optional
+            The communication method.  Supported values are 'serial' and 'tcp'
+        baudrate : int, optional
+            The serial baudrate (if using comm_method='serial') to use
+        timeout : float, optional
+            The read timeout for serial operations (if using comm_method='serial').  Defaults to 0.25
+        ip_address : str, optional
+            The IP address (if using comm_method='tcp') of the device
+        ip_port : int, optional
+            The port number (if using comm_method='tcp') to use.  Defaults to 5000.
+        sw : drivers.switcher.Switcher, optional
+            A reference back to a Switcher object that uses this SwitcherInterface
+        inputs : int, optional
+            Number of inputs this device has.  Defaults to 2.  The driver does not have a way of automatically
+            determining this, so it should be provided if it differs from 2.
+        outputs : int, optional
+            Number of outputs this device has.  More than 1 indicates it's a matrix switcher.  Defaults to None.
+            If left as the default, the driver will attempt to figure out how many outputs there are
+            using whatever routing queries the switcher supports.
+        """
         try:
             self.switcher = sw
-
-            self._power_status = None
-            self._input_status = None
-            self._av_mute = None
 
             if comm_method == 'serial':
                 logger.debug('__init__(): Establishing RS-232 connection on device %s @ %d',
@@ -187,7 +236,6 @@ class KramerP3000(SwitcherInterface):
                 self.comms = self.Comms()
                 self.comms.ip_address = ip_address
                 self.comms.ip_port = ip_port
-                self.comms.ip_timeout = ip_timeout
                 self.comms.connection = create_connection((ip_address, ip_port), timeout=None)
                 self.comms.connection.setblocking(False)
                 logger.debug('__init__(): Connection established')
@@ -200,14 +248,23 @@ class KramerP3000(SwitcherInterface):
             self.comms.connection.close()
             logger.debug('__init__(): Connection closed')
 
-        # get number of inputs.  not all switchers support a way to query this so it's better if we pass it in
+        # Get number of inputs.  Not all switchers support a way to query this (which is why
+        # I didn't bother with writing a method) so it's better if we pass it in.  There is
+        # a command called '#INFO-IO?' that is only available on certain devices.
         self.inputs = inputs
 
-        # is this a matrix switcher?  we should be able to tell by the length of the input status response
-        input_status = self.input_status
-        self.outputs = len(input_status)
+        # if number of outputs is not provided, we should be able
+        # to tell by the length of the list returned by input_status
+        if not outputs:
+            input_status = self.input_status
+            self.outputs = len(input_status)
 
     def open_connection(self):
+        """Open the connection for read/write
+
+        For serial connections, the connection is merely reopened.  For TCP sockets, a new connection
+        must be created and returned.
+        """
         if isinstance(self.comms.connection, Serial):
             self.comms.connection.open()
         elif isinstance(self.comms.connection, socket):
@@ -215,29 +272,24 @@ class KramerP3000(SwitcherInterface):
                 (self.comms.ip_address, self.comms.ip_port), timeout=None
             )
             self.comms.connection.setblocking(False)
-        logger.debug('Connection opened')
 
     def close_connection(self):
+        """Close the connection
+        """
         self.comms.connection.close()
-        logger.debug('Connection closed')
-
-    def power_on(self):
-        logger.debug('power_on(): Unsupported')
-        return None
-
-    def power_off(self):
-        logger.debug('power_off(): Unsupported')
-        return None
-
-    @property
-    def power_status(self):
-        logger.debug('power_status: Unsupported')
-        return None
 
     def _try_cmd(self, cmd):
         """Helper method for input_status and select_input
-        :param cmd The command string (bytes) to call
-        :return Tuple of error received (if any) and response.  Tuple of True and the response if no errors.
+
+        Parameters
+        ----------
+        cmd : bytes|str
+            The command string (bytes) to call.  A str will be converted to bytes.
+
+        Returns
+        -------
+        Tuple of error received (if any) and the response from the switcher for parsing.
+        If no error occurred, returns tuple of True and the response.
         """
         if isinstance(cmd, str):
             cmd = bytes(cmd, 'ascii')
@@ -307,7 +359,7 @@ class KramerP3000(SwitcherInterface):
                     continue
                 elif result == self.Error.PARAM_OUT_OF_RANGE:
                     raise ValueError('select_input(): Input or output number out of range - '
-                                     'input={}, output={}', input_, output)
+                                     'input={}, output={}'.format(input_, output))
                 elif result == self.Error.SYNTAX:
                     raise SyntaxError('select_input(): KramerP3000 syntax error: {}'.format(cmd))
                 # if result is True (or just not b'') and no ERR is reported, we probably succeeded
@@ -408,8 +460,27 @@ class KramerP3000(SwitcherInterface):
         finally:
             self.close_connection()
 
+    def power_on(self):
+        """Unsupported
+        """
+        logger.debug('power_on(): Unsupported')
+        return None
+
+    def power_off(self):
+        """Unsupported
+        """
+        logger.debug('power_off(): Unsupported')
+        return None
+
+    @property
+    def power_status(self):
+        """Unsupported
+        """
+        logger.debug('power_status: Unsupported')
+        return None
+
     @property
     def av_mute(self):
         # todo: maybe think about implementing this (or don't). it's queried per output so that's a pain of course
-        logger.warning('av_mute: Not implemented at this time')
+        logger.warning('av_mute: Not implemented')
         return False
