f12d243569f94837965c4cfbda201d1ef3a1b833
tannmatter <tannmatter@gmail.com>
2020-11-13 11:38:28 -0600

    more KramerVP734 debugging

--- drivers/switcher/kramervp734.py
+++ drivers/switcher/kramervp734.py
@@ -16,12 +16,12 @@ logger.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.ERROR)
+console_handler.setLevel(logging.DEBUG)
 console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 
 file_handler = logging.FileHandler('avc.log')
-file_handler.setLevel(logging.WARNING)
+file_handler.setLevel(logging.DEBUG)
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 
