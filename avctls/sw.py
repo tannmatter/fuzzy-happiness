b2aa62eed187e450e469298d400d53d611abb263
tannmatter <tannmatter@gmail.com>
2020-11-30 12:16:49 -0600

    silly copy paste

--- avctls/sw.py
+++ avctls/sw.py
@@ -2,8 +2,6 @@ from flask import Blueprint, render_template, current_app
 
 sw = Blueprint('sw', __name__, url_prefix='/sw')
 
-# todo: this isn't doing what i thought it would.  the routes are the same as the projector...
-# I thought url_prefix above would cause it to make to /sw/input, etc?
 
 @sw.route('/')
 def sw_index():
@@ -12,7 +10,7 @@ def sw_index():
 
 @sw.route('/input')
 def sw_input_status():
-    status = current_app.room.pj.interface.input_status
+    status = current_app.room.sw.interface.input_status
     if not status:
         return "UNDEFINED"
     return status
@@ -20,20 +18,20 @@ def sw_input_status():
 
 @sw.route('/input/<inp>')
 def sw_select_input(inp):
-    status = current_app.room.pj.interface.select_input(inp)
+    status = current_app.room.sw.interface.select_input(inp)
     return status
 
 
 # for debugging
 @sw.route('/inputs')
 def sw_get_inputs():
-    inputs = current_app.room.pj.interface.inputs
+    inputs = current_app.room.sw.interface.inputs
     return inputs
 
 
 @sw.route('/power')
 def sw_get_power_state():
-    status = current_app.room.pj.interface.power_status
+    status = current_app.room.sw.interface.power_status
     if not status:
         return "UNDEFINED"
     return status
@@ -41,12 +39,12 @@ def sw_get_power_state():
 
 @sw.route('/power/<state>')
 def sw_set_power_state(state):
-    projector = current_app.room.pj.interface
+    switcher = current_app.room.sw.interface
     if state == 'on' or state == '1':
-        if projector.power_on():
+        if switcher.power_on():
             return 'On'
     elif state == 'off' or state == '0':
-        if projector.power_off():
+        if switcher.power_off():
             return 'Off'
     else:
         return 'parameter {} invalid'.format(state)
